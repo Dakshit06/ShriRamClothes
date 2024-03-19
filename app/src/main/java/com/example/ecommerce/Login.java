c392908ed77eb79e7b2a417ae6ce3fe76cb57513
Dakshit06 <dakshit06@gmail.com>
2024-03-19 23:59:53 +0530

    Email verify done

--- app/src/main/java/com/example/ecommerce/Login.java
+++ app/src/main/java/com/example/ecommerce/Login.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -9,86 +10,110 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.Firebase;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.regex.Pattern;
-
 public class Login extends AppCompatActivity {
-private FirebaseAuth auth;
-private EditText loginEmail,loginPassword;
-private TextView signupRedirectText;
-private Button loginButton;
+    private FirebaseAuth auth;
+    private EditText loginEmail, loginPassword;
+    private TextView signupRedirectText, forgotTextLink;
+    private Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
-    auth = FirebaseAuth.getInstance();
-    loginEmail=findViewById(R.id.login_email);
-    loginPassword=findViewById(R.id.login_password);
-    loginButton=findViewById(R.id.login_button);
-    signupRedirectText=findViewById(R.id.signupRedirectText);
+        auth = FirebaseAuth.getInstance();
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.login_button);
+        signupRedirectText = findViewById(R.id.signupRedirectText);
+        forgotTextLink = findViewById(R.id.forgotPassword);
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = loginEmail.getText().toString();
+                String pass = loginPassword.getText().toString();
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if (!pass.isEmpty()) {
+                        auth.signInWithEmailAndPassword(email, pass)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(Login.this, MainActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(Exception e) {
+                                        Toast.makeText(Login.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
 
+                    } else {
+                        loginPassword.setError("Password Cannot be Empty");
+                    }
 
-    loginButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            String email= loginEmail.getText().toString();
-            String pass=loginPassword.getText().toString();
-            if(!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-                if(!pass.isEmpty()){
-                    auth.signInWithEmailAndPassword(email,pass)
-                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                                @Override
-                                public void onSuccess(AuthResult authResult) {
-                                    Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                                    startActivity(new Intent(Login.this,MainActivity.class));
-                                    finish();
-                                }
-
-                            }).addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(Login.this, "Login Failed", Toast.LENGTH_SHORT).show();
-
-                                }
-                            });
-
+                } else if (email.isEmpty()) {
+                    loginEmail.setError("Email Cannot be Empty");
+                } else {
+                    loginEmail.setError("Please Enter the Valid email");
                 }
-                else{
-                    loginPassword.setError("Password Cannot be Empty");
-                }
-
 
             }
-            else if(email.isEmpty()){
-                loginEmail.setError("Email Cannot be Empty");
-            }
-            else{
-                loginEmail.setError("Please Enter the Valid email");
-            }
+        });
 
-        }
-    });
-    signupRedirectText.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(Login.this,Register.class);
-            startActivity(intent);
-        }
-    });
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+        forgotTextLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText resetMail = new EditText(v.getContext());
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password?");
+                passwordResetDialog.setMessage("Enter Your Email To Received Reset Link.");
+                passwordResetDialog.setView(resetMail);
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //extract the email and send reset link
+                        String mail = resetMail.getText().toString();
+                        auth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Login.this, "Reset Link Sent To Your Email", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(Exception e) {
+                                Toast.makeText(Login.this, "Error! Reset Link is Not Send" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //close the dialog
+                    }
+                });
+                // Show the password reset dialog
+                passwordResetDialog.show();
+            }
+        });
     }
 }
